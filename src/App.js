247d18b6505a23f591770f73c918eab6b4f3a83e
Liubomyr Fedoryshyn <39272268+LiubomyrFedoryshyn@users.noreply.github.com>
2020-10-30 11:53:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React from "react"; // import React, { useState } from 'react'; - so you can use useState manualy; 
+
 import "./App.css";
 
-function Todo({ todo, index, completeTodo, removeTodo }) {
+function Todo({ todo, index, completeTodo, removeTodo }) { // It woould be better if you move this function to a separate component;
   return (
     <div
-      className="todo"
+      className="todo" //  className={'todo', todo.isComlete && 'line-througth'}, and write to class 'line-througth' some styles in css
       // when the item is crossed
-      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }} // inline-styles, bad practise
     >
       {todo.text}
       <div>
@@ -21,15 +22,15 @@ function Todo({ todo, index, completeTodo, removeTodo }) {
 // start with an empty state for an input field
 // be able to update the form by setting the state
 // handle the submit
-function TodoForm({ addTodo }) {
+function TodoForm({ addTodo }) { // also, separate component
   // srtting the state 
-  const [value, setValue] = React.useState("");
+  const [value, setValue] = React.useState(""); // const [value, setValue] = useState(""); - if you have imports above
 
   // adding item to the list by pressing ENTER 
   // or add nothing if nothing is typed in the field
   const handleSubmit = e => {
     e.preventDefault();
-    if (!value) return;
+    if (!value) return; // if (value) { addTodo(value); setValue('') };
     addTodo(value);
     setValue("");
   };
@@ -48,7 +49,7 @@ function TodoForm({ addTodo }) {
 
 function App() {
   // totos - name of the state, setTodos - used to set the state.
-  const [todos, setTodos] = React.useState([
+  const [todos, setTodos] = React.useState([ // same imports
     {
       text: "Add an item to your TODO List",
       // set that to false to begin with and, when prompted, it will change that to true
@@ -65,13 +66,13 @@ function App() {
 
   const completeTodo = index => {
     const newTodos = [...todos];
-    newTodos[index].isCompleted = true; 
+    newTodos[index].isCompleted = true; //  newTodos[index].isCompleted = !newTodos[index].isCompleted, toggler is better here, so we can undo some completed stuff
     setTodos(newTodos);
   };
   // COMPONENT FOR DELETING ITEMS
   const removeTodo = index => {
     const newTodos = [...todos];
-    newTodos.splice(index, 1);
+    newTodos.splice(index, 1); //it would be better to use a 'filter' method instead of 'slice' 
     setTodos(newTodos);
   };
 
